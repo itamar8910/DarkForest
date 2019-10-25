eff6da241ff4f04a96e8e731d85fc67ac584f72d
Itamar Shenhar <itamar8910@gmail.com>
2019-10-26 01:12:49 +0300

    KeyBoard:: add basic key map for scan code set 1

--- kernel/drivers/PS2Keyboard.cpp
+++ kernel/drivers/PS2Keyboard.cpp
@@ -4,6 +4,7 @@
 #include "logging.h"
 #include "IO.h"
 #include "Kassert.h"
+#include "bits.h"
 
 #define PS2_IRQ1 1
 #define PS2_IRQ2 12
@@ -17,56 +18,149 @@
 
 #define PS2_CMD_ECHO 0xEE
 
+#define SCAN_CODE_MULTIBYTE 0xE0
+
 
 ISR_HANDLER(ps2_keyboard)
 void isr_ps2_keyboard_handler(RegisterDump& regs) {
     (void)regs;
     kprintf("PS2Keyboard ISR\n");
     u8 res = IO::inb(PS2_DATA_PORT);
-    kprintf("keyboard data: 0x%x\n", res);
+    kprintf("keyboard byte: 0x%x\n", res);
+    PS2Keyboard::the().received_scan_byte(res);
     PIC::eoi(PS2_IRQ1);
 }
 
 // TODO: split this to a generic PS2 handler
 // PS2Keyboard should use the PS2 handler instead if writing to raw IO ports
 
-namespace PS2Keyboard {
 
-    u8 get_status() {
-        return IO::inb(PS2_STATUS_PORT);
+u8 PS2Keyboard::get_status() {
+    return IO::inb(PS2_STATUS_PORT);
+}
+
+void PS2Keyboard::poll_for_write() {
+    for(size_t i = 0; i < 10; i++) {
+        if(!(get_status() & (STATUS_INPUT))) {
+            return;
+        }
     }
+    ASSERT_NOT_REACHED("PS2Keyboard: poll for write exceeded");
+}
+void PS2Keyboard::poll_for_read() {
+    for(size_t i = 0; i < 10; i++) {
+        if((get_status() & (STATUS_OUTPUT))) {
+            return;
+        }
+    }
+    ASSERT_NOT_REACHED("PS2Keyboard: poll for write exceeded");
+}
 
-    void poll_for_write() {
-        for(size_t i = 0; i < 10; i++) {
-            if(!(get_status() & (STATUS_INPUT))) {
+static PS2Keyboard* s_the = nullptr;
+
+void PS2Keyboard::initialize() {
+
+    u8 val = get_status();
+    kprintf("PS2 status: %d\n", val);
+    poll_for_write();
+    IO::outb(PS2_DATA_PORT, PS2_CMD_ECHO);
+    poll_for_read();
+    u8 res = IO::inb(PS2_DATA_PORT);
+    kprintf("PS2 echo res: 0x%x\n", res);
+    ASSERT(res == PS2_CMD_ECHO); // According to specs could also be "RESEND"
+    
+    s_the = new PS2Keyboard();
+
+    register_interrupt_handler(IRQ_ISRS_BASE + PS2_IRQ1, isr_ps2_keyboard_entry);
+
+    PIC::enable_irq(PS2_IRQ1);
+    PIC::enable_irq(PS2_IRQ2);
+
+}
+
+
+PS2Keyboard& PS2Keyboard::the() {
+    ASSERT(s_the != nullptr);
+    return *s_the;
+
+}
+
+PS2Keyboard::PS2Keyboard() 
+    : current_keycode_bytes{0},
+      current_keycode_byte_idx{0},
+      keycodes_buffer_idx{0} {}
+
+
+void PS2Keyboard::received_scan_byte(u8 val) {
+    current_keycode_bytes[current_keycode_byte_idx++] = val;
+    switch(current_keycode_byte_idx) {
+        case 1:
+            {
+            if(val == SCAN_CODE_MULTIBYTE) {
                 return;
             }
-        }
-        ASSERT_NOT_REACHED("PS2Keyboard: poll for write exceeded");
-    }
-    void poll_for_read() {
-        for(size_t i = 0; i < 10; i++) {
-            if((get_status() & (STATUS_OUTPUT))) {
-                return;
+            bool released = get_bit(val, 7);
+            val &= ~(1<<7); // clear 'pressed/realeased bit'
+            KeyCode key_code = KeyCode::from_single(val);
+            KeyState key_state = KeyState(key_code, released, false);
+            kprintf("key: %c\n", key_state.to_ascii());
+            keycodes_buffer[keycodes_buffer_idx++] = key_state;
+            keycodes_buffer_idx %= KEYCODES_BUFFER_LEN;
+            current_keycode_byte_idx = 0;
+            break;
             }
-        }
-        ASSERT_NOT_REACHED("PS2Keyboard: poll for write exceeded");
-    }
+        case 2:
+            break;
+        default:
+            ASSERT_NOT_REACHED("PS2Keyboard: invalid scan code length");
+    };
+}
 
-    void initialize() {
+#define BIT7 128
+enum NonAsciiKeys {
+    ESC = BIT7 ,
+    F1,
+    F2,
+    F3,
+    F4,
+    F5,
+    F6,
+    F7,
+    F8,
+    F9,
+    F10,
+    BackSpace,
+    TAB,
+    ENTER,
+    L_CTRL,
+    L_SHIFT,
+    R_SHIFT,
+    L_ALT,
+    CapsLock,
+    NumLock,
+    ScrollLock,
+};
 
-        u8 val = get_status();
-        kprintf("PS2 status: %d\n", val);
-        poll_for_write();
-        IO::outb(PS2_DATA_PORT, PS2_CMD_ECHO);
-        poll_for_read();
-        u8 res = IO::inb(PS2_DATA_PORT);
-        kprintf("PS2 echo res: 0x%x\n", res);
-        ASSERT(res == PS2_CMD_ECHO); // According to specs could also be "RESEND"
-        
-        register_interrupt_handler(IRQ_ISRS_BASE + PS2_IRQ1, isr_ps2_keyboard_entry);
+const u8 key_map_set1[128] = {
+    0, ESC, 
+    '1', '2', '3', '4' ,'5' ,'6', '7', '8', '9', '0', // 11 
+    '-', '=', BackSpace, TAB, //15
+    'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', // 0x1b
+    ENTER, L_CTRL, 
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k' , 'l', ';', // 0x27
+    '\'', '`', L_SHIFT, '\\', // 0x2b
+    'z', 'x' ,'c', 'v', 'b', 'n', 'm', ',', '.', '/', // 0x35
+    R_SHIFT, 0, L_ALT, ' ', CapsLock, // 0x3a
+    F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, // 0x44
+    NumLock, ScrollLock
+};
 
-        PIC::enable_irq(PS2_IRQ1);
-        PIC::enable_irq(PS2_IRQ2);
-    }
+KeyCode KeyCode::from_single(u8 val) {
+    return key_map_set1[val];
+}
+
+char KeyState::to_ascii() {
+    ASSERT(!get_bit(keycode.data, 7), "KeyState::to_ascii invalid data");
+    // TODO: use maps based on shift/CapsLock
+    return (char) (keycode.data);
 }
--- kernel/drivers/PS2Keyboard.h
+++ kernel/drivers/PS2Keyboard.h
@@ -2,7 +2,45 @@
 
 #include "types.h"
 
-namespace PS2Keyboard {
-    inline u8 get_status();
-    void initialize();
-}
+// the longer getcode has 6 bytes
+#define MAX_NUM_KEYCODE_BYTES 6
+#define KEYCODES_BUFFER_LEN 20
+
+
+struct KeyCode {
+    // upper 3 bits = row, lower 5 bits = col
+    u8 data;
+    KeyCode(u8 data): data(data) {}
+    static KeyCode from_single(u8 val);
+};
+
+struct KeyState {
+    KeyCode keycode;
+    bool released; // false for pressed, true for released
+    bool shift_pressed;
+    KeyState(): keycode(0), released(false), shift_pressed(false) {}
+    KeyState(KeyCode keycode, bool released, bool shift_pressed)
+        : keycode(keycode), released(released), shift_pressed(shift_pressed)
+        {}
+    char to_ascii();
+};
+
+
+class PS2Keyboard {
+public:
+    static void initialize();
+    static PS2Keyboard& the();
+    void received_scan_byte(u8 val);
+private:
+    PS2Keyboard();
+    static inline u8 get_status();
+    static void poll_for_write();
+    static void poll_for_read();
+
+    u8 current_keycode_bytes[MAX_NUM_KEYCODE_BYTES];
+    u8 current_keycode_byte_idx;
+
+    KeyState keycodes_buffer[KEYCODES_BUFFER_LEN]; // circuler buffer that stores KeyCodes
+    u8 keycodes_buffer_idx;
+
+};
