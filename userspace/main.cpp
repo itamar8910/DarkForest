f3904279b8fe526445aeb1929e9b083b895e4987
Itamar <itamar8910@gmail.com>
2019-11-08 22:50:08 +0200

    usersapce binaries: _start calls exit() syscall after main returns

--- userspace/main.cpp
+++ userspace/main.cpp
@@ -1,9 +1,15 @@
 
 
-int main() {
+int func() {
     asm volatile(
+        // "1:\n"
         "mov $3, %eax\n"
         "int $0x80\n"
+        // "jmp 1b\n"
     );
     return 0;
+}
+
+int main() {
+    return func();
 }
