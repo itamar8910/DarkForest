e7124515707fbf27da7c59981dc9e32480309db0
Itamar <itamar8910@gmail.com>
2019-11-09 21:55:29 +0200

    syscall wrappers: fix callee-saved ebx
    ebx should be callee saved, so
    we have to put it in the clobbered list
    
    this took way too long to debug haha

--- libc/stdio.cpp
+++ libc/stdio.cpp
@@ -1,5 +1,7 @@
 #include "stdio.h"
 #include "syscalls.h"
+#include "stdarg.h"
+#include "printf.h"
 
 void puts(const char* str) {
     asm volatile(
@@ -8,5 +10,26 @@ void puts(const char* str) {
         "int $0x80\n"
         :
         : "rm"(str), "i"(Syscall::Kprintf)
+        : "ebx", "eax"
     );
+}
+
+void putc(char c) {
+    asm volatile(
+        "movl %0, %%ebx\n"
+        "movl %1, %%eax\n"
+        "int $0x80\n"
+        :
+        : "rm"(c), "i"(Syscall::Kputc)
+        : "ebx", "eax"
+    );
+
+}
+
+void printf(const char* fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    printf_internal(putc, fmt, args);
+    va_end(args);
+
 }
--- libc/unistd.cpp
+++ libc/unistd.cpp
@@ -3,22 +3,28 @@
 
 [[noreturn]] void exit(int status) {
     asm volatile(
+        // "pushl %%ebx\n" // ebx is callee saved
         "movl %0, %%ebx\n"
         "movl %1, %%eax\n"
         "int $0x80\n"
+        // "popl %%ebx\n"
         :
         : "rm"(status), "i"(Syscall::Exit)
+        : "ebx", "eax"
     );
     for(;;);
 }
 
 void sleep_ms(u32 ms) {
     asm volatile(
+        // "pushl %%ebx\n" // ebx is callee saved
         "movl %0, %%ebx\n"
         "movl %1, %%eax\n"
         "int $0x80\n"
+        // "popl %%ebx\n"
         :
         : "rm"(ms), "i"(Syscall::SleepMs)
+        : "ebx", "eax"
     );
 
 }
