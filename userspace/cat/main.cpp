6d4b732e34891940c3c7f43e69f205d4035b9bee
Itamar <itamar8910@gmail.com>
2019-12-13 17:16:37 +0200

    userspace: pass argv, argc on stack

--- userspace/cat/main.cpp
+++ userspace/cat/main.cpp
@@ -4,8 +4,6 @@
 #include "stdio.h"
 #include "constants.h"
 
-#define USERSPACE_STACK 0xb0000000
-
 void print_hello_text() {
     int fd = std::open("/initrd/a.txt");
     ASSERT(fd>=0);
@@ -18,16 +16,10 @@ void print_hello_text() {
     printf("%s\n", buff);
 }
 
-int main() {
-    // std::sleep_ms(500);
+int main(char** argv, size_t argc) {
     print_hello_text();
-   u32 user_args_end = USERSPACE_STACK + PAGE_SIZE*2;
-   u32 user_args_start = user_args_end - PAGE_SIZE;
-   char*** argv_ptr = (char***) (user_args_start+4);
-   size_t* argc_ptr = (size_t*) (user_args_start+8);
-   char** argv = *argv_ptr;
-   size_t argc = *argc_ptr;
-   kprintf("argc: %d\n", argc);
-   kprintf("argv[0]: %s\n", argv[0]);
+    kprintf("argc: %d\n", argc);
+    if(argc > 0)
+        kprintf("argv[0]: %s\n", argv[0]);
     return 0;
 }
