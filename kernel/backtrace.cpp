041719ae6ad2aae97b7a9f703ce776859a7e8e8d
Itamar <itamar8910@gmail.com>
2019-12-20 16:38:59 +0200

    backtrace: check if symbol was found

--- kernel/backtrace.cpp
+++ kernel/backtrace.cpp
@@ -10,7 +10,6 @@ using namespace Backtrace;
 
 
 void Backtrace::print_backtrace(u32 eip, u32 ebp) {
-   // kprintf("backtrace: eip:0x%x, ebp:0x%x\n", eip, ebp);
    auto* source_info = KernelSymbols::the().lines_map().locate(eip);
    ASSERT(source_info != nullptr);
    kprint("Generated from:\n");
@@ -21,8 +20,12 @@ void Backtrace::print_backtrace(u32 eip, u32 ebp) {
    kprintf(">> 0x%x %s\n", symbol_info->address, symbol_info->symbol_name.c_str());
    for(u32* stack = (u32*) ebp; stack[0] != 0; stack = (u32*) stack[0]) {
       u32 addr_in_next_function = stack[1];
-      // kprintf("0x%x\n", addr_in_next_function);
       auto* symbol_info = KernelSymbols::the().symbols_map().locate(addr_in_next_function);
+      if(symbol_info == nullptr) {
+         kprintf("<Backtrace: couldn't find symbol for: 0x%x>\n", addr_in_next_function);
+         break;
+      }
       kprintf(">> 0x%x %s\n", symbol_info->address, symbol_info->symbol_name.c_str());
    }
+   kprintf("<End of Backtrace>\n");
 }
