8a3c5cb6de29de58f2a5e54331d0ec2c69658f5f
Itamar <itamar8910@gmail.com>
2022-10-07 10:30:00 +0300

    rtl8139 - hide debug prints behind flag

--- kernel/drivers/RTL8139NetworkCard.cpp
+++ kernel/drivers/RTL8139NetworkCard.cpp
@@ -21,6 +21,8 @@
 #define TRANSMIT_START_ADDRESS_0 0x20
 #define TRANSMIT_STATUS_0 0x10
 
+// #define RTL8139_DBG
+
 static RTL8139NetworkCard* s_the = nullptr;
 
 RTL8139NetworkCard& RTL8139NetworkCard::the()
@@ -44,16 +46,24 @@ void RTL8139NetworkCard::initialize()
 ISR_HANDLER(rtl8139)
 void isr_rtl8139_handler(RegisterDump& regs) {
     (void)regs;
+#ifdef RTL8139_DBG
     kprintf("isr_rtl8139_handler interrupt!\n~~~~~~~\n");
+#endif
 
     auto interrupt_status = IO::in16(RTL8139NetworkCard::the().io_base_address() + 0x3E);
+#ifdef RTL8139_DBG
     kprintf("interrupt status: 0x%x\n", interrupt_status);
+#endif
 
     if (interrupt_status & 0x4) {
+#ifdef RTL8139_DBG
         kprintf("Transmit OK\n");
+    #endif
     }
     if (interrupt_status & 0x1) {
+#ifdef RTL8139_DBG
         kprintf("Recv OK\n");
+    #endif
         RTL8139NetworkCard::recv_packet_static();
     }
 
@@ -74,18 +84,25 @@ void RTL8139NetworkCard::recv_packet()
     auto* current_data = m_recv_buffer->data() + m_recv_buffer_offset;
     uint16_t status = *(uint16_t*)(current_data);
     uint16_t length = *(uint16_t*)(current_data + 2);
+
+#ifdef RTL8139_DBG
     kprintf("status: %d, length: %d\n", status, length);
+#endif
 
     if (!(status & 1))
     {
+#ifdef RTL8139_DBG
         kprintf("rtl8139: recv error\n");
+#endif
         return;
     }
 
     auto* packet = current_data + 4;
     auto packet_size = length - 4;
+#ifdef RTL8139_DBG
     kprintf("packet:\n");
     print_hexdump(packet, packet_size);
+#endif
 
     Network::NetworkManager::the().on_packet_received(packet, packet_size);
 
@@ -102,10 +119,7 @@ void RTL8139NetworkCard::recv_packet()
 RTL8139NetworkCard::RTL8139NetworkCard(PCIBus::PciDeviceMetadata device_metadata)
      : m_device_metadata(device_metadata)
 {
-   kprintf("RTL8139NetworkCard::ctor()\n");
-
    m_irq_nuber = device_metadata.irq_number;
-   kprintf("irq number: %d\n", device_metadata.irq_number);
 
    verify_bus_mastering();
    turn_on();
@@ -154,16 +168,24 @@ void RTL8139NetworkCard::transmit(u8* data, size_t size)
     current_status = (current_status & 0xfffff000) | size;
     current_status = set_bit(current_status, 13, 0); // begin transmit
 
+#ifdef RTL8139_DBG
     kprintf("beginning to transmit\n");
+#endif
     IO::out32(m_device_metadata.io_base_address + TRANSMIT_STATUS_0 + m_current_transmit_index*4, current_status);
 
     while (get_bit(IO::in32(m_device_metadata.io_base_address + TRANSMIT_STATUS_0 + m_current_transmit_index*4), 15) != 1) {
+#ifdef RTL8139_DBG
         kprintf("waiting for transmit\n");
+#endif
     }
+#ifdef RTL8139_DBG
     kprintf("transmit done\n");
+#endif
 
     current_status = IO::in32(m_device_metadata.io_base_address + TRANSMIT_STATUS_0 + m_current_transmit_index*4);
+#ifdef RTL8139_DBG
     kprintf("current send status: 0x%x\n", current_status);
+#endif
 
     m_current_transmit_index = (m_current_transmit_index + 1) % NUM_SEND_BUFFERS;
 }
@@ -197,7 +219,7 @@ void RTL8139NetworkCard::init_recv_buffer()
 
     // NOTE: Terrible hack.
     // Allocate twice to actually get contiguous physical memory
-    // (by luck, since in the 2nd time around we don't need to allocate a frame for the page table)
+    // (hack, works because in the 2nd time around we don't need to allocate a frame for a new page table)
 
     auto temp_buffer = BigBuffer::allocate(num_pages * PAGE_SIZE, false);
     m_recv_buffer = BigBuffer::allocate(num_pages * PAGE_SIZE, true);
@@ -205,16 +227,12 @@ void RTL8139NetworkCard::init_recv_buffer()
     auto physical_address_of_recv_buffer = MemoryManager::the().get_physical_address((u32)(m_recv_buffer->data()));
 
     IO::out32(m_device_metadata.io_base_address + RECEIVEBUFFER_START_REG, physical_address_of_recv_buffer);
-    // kprintf("receive buffer: %p\n", m_recv_buffer->data());
-    // m_recv_buffer->data()[0] = 1;
-    // kprintf("receive buffer[0]: %d\n", m_recv_buffer->data()[0]);
 }
 
 
 void RTL8139NetworkCard::verify_bus_mastering()
 {
     uint16_t config_command_reg = PCIBus::config_read_short(m_device_metadata.pci_bus, m_device_metadata.pci_slot, 0, 0x4);
-    kprintf("config_command: 0x%x\n", config_command_reg);
 
     config_command_reg |= (1 << 0);
     config_command_reg |= (1 << 2); // Enable "bus mastering"
@@ -222,7 +240,6 @@ void RTL8139NetworkCard::verify_bus_mastering()
     PCIBus::config_write_short(m_device_metadata.pci_bus, m_device_metadata.pci_slot, 0, 0x4, config_command_reg);
 
     config_command_reg = PCIBus::config_read_short(m_device_metadata.pci_bus, m_device_metadata.pci_slot, 0, 0x4);
-    kprintf("config_command after enabling bus mastering: 0x%x\n", config_command_reg);
 
     ASSERT(config_command_reg & (1<<2)); // Ensure "bus mastering" is on
 }
@@ -237,9 +254,14 @@ void RTL8139NetworkCard::software_reset()
     IO::outb(m_device_metadata.io_base_address + COMMAND_REG, 0x10);
     uint32_t reset_bit = 0x4;
     while ((IO::inb(m_device_metadata.io_base_address + COMMAND_REG) & (1<<reset_bit)) != 0) {
+
+#ifdef RTL8139_DBG
         kprintf("waiting for reset\n");
+#endif
     }
+#ifdef RTL8139_DBG
     kprintf("done waiting for reset\n");
+#endif
 }
 
 void RTL8139NetworkCard::init_send_buffers()
