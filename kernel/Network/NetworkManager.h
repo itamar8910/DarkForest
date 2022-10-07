dc4e2c734b75a61794e460785a215b2fc0915e76
Itamar <itamar8910@gmail.com>
2022-10-07 10:30:16 +0300

    hold our IP & gateway's IP in NetworkManger

--- kernel/Network/NetworkManager.h
+++ kernel/Network/NetworkManager.h
@@ -16,10 +16,15 @@ public:
     void on_packet_received(u8* packet, size_t size);
     void transmit(const u8* packet, size_t size);
 
+    IPV4 our_ip() const { return m_our_ip; }
+    IPV4 gateway_ip() const { return m_gateway_ip; }
+
 private:
     NetworkManager(MAC our_mac);
 
     MAC m_our_mac {};
+    IPV4 m_our_ip {};
+    IPV4 m_gateway_ip {};
 };
 
 }
